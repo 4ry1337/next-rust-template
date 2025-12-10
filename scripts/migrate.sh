1d8ef88114a87affb7b8fe789559c29bedbe6f2e
Rakhat Yskak <yskak.rakhat@gmail.com>
2025-12-11 00:37:47 +0500

    [scripts] migrate database

---
+++ scripts/migrate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "cargo install sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
